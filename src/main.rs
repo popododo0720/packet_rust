210b0aec92e1513c597733a2e194a6fd5f4f0e85
popododo0720 <popododo0720@naver.com>
2025-03-04 07:47:15 +0000

    first

---
+++ src/main.rs
@@ -0,0 +1,49 @@
+extern crate pnet;
+
+use pnet::datalink::{self, NetworkInterface};
+use pnet::datalink::Channel::Ethernet;
+use pnet::packet::{Packet, MutablePacket};
+use pnet::packet::ethernet::{EthernetPacket, MutableEthernetPacket};
+use pnet::packet::ipv4::{Ipv4Packet};
+use pnet::packet::udp::{UdpPacket};
+
+use std::env;
+
+// Invoke as echo <interface name>
+fn main() {
+    let interface_name = env::args().nth(1).unwrap();
+    let interface_names_match =
+        |iface: &NetworkInterface| iface.name == interface_name;
+
+    // Find the network interface with the provided name
+    let interfaces = datalink::interfaces();
+    let interface = interfaces.into_iter()
+                              .filter(interface_names_match)
+                              .next()
+                              .unwrap();
+
+    // Create a new channel, dealing with layer 2 packets
+    let (mut tx, mut rx) = match datalink::channel(&interface, Default::default()) {
+        Ok(Ethernet(tx, rx)) => (tx, rx),
+        Ok(_) => panic!("Unhandled channel type"),
+        Err(e) => panic!("An error occurred when creating the datalink channel: {}", e)
+    };
+
+    loop {
+        match rx.next() {
+            Ok(packet) => {
+                let packet = EthernetPacket::new(packet).unwrap();
+
+                if let Some(ipv4_packet) = Ipv4Packet::new(packet.payload()) {
+                    if ipv4_packet.get_next_level_protocol() == pnet::packet::ip::IpNextHeaderProtocols::Udp
+ {
+    println!("UDP");
+ }                }
+            },
+            Err(e) => {
+                // If an error occurs, we can handle it here
+                panic!("An error occurred while reading: {}", e);
+            }
+        }
+    }
+}
